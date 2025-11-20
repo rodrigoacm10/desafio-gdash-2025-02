6880356f15cdedc807dde4fb00542e04e14b609e
Rodrigo Andrade <rodrigoacm10@gmail.com>
2025-11-20 08:19:56 -0300

    feat: backend base config

---
+++ backend/src/main.ts
@@ -0,0 +1,22 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.setGlobalPrefix('api');
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  const port = process.env.API_PORT || 3000;
+  await app.listen(port);
+  console.log(`API running on port ${port}`);
+}
+bootstrap();
