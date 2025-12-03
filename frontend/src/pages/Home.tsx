42b892927ae4891fbea9f55482d288b4485338a1
Rodrigo Andrade <rodrigoacm10@gmail.com>
2025-12-02 23:14:20 -0300

    feat: apresentation page

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom'
 
 export const Home = () => {
   return (
-    <div className="bg-black min-h-screen">
+    <div className="bg-black min-h-screen flex flex-col">
       <div className="bg-white flex justify-center">
         <div className="max-w-[1250px] w-full px-5 py-2.5 flex items-center justify-between">
           <p className="text-black">
@@ -30,6 +30,19 @@ export const Home = () => {
               </Button>
             </Link>
           </div>
+        </div>
+      </div>
+
+      <div className="flex-1 flex items-center justify-center relative bg-[url('/home-background.jpg')] bg-cover bg-center bg-no-repeat">
+        <div className="absolute inset-0 bg-gradient-to-b from-white/10  to-black" />
+
+        <div className="z-50 text-5xl font-bold flex ">
+          <p className="text-[#156e6a]">
+            <span className="bg-white rounded-2xl px-4 py-2 bg-gradient-to-b from-white/10  to-black/30">
+              Weather
+            </span>{' '}
+            <span className="text-white">Manegement</span>
+          </p>
         </div>
       </div>
     </div>
